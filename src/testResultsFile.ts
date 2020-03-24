4fcd236a1604b30c3b3dc173a5d121a0e2791703
Marek Linka <marek.linka@outlook.com>
2020-03-24 10:51:55 +0800

    Rewrite result file loading to use async

--- src/testResultsFile.ts
+++ src/testResultsFile.ts
@@ -3,32 +3,29 @@ import { DOMParser } from 'xmldom';
 import { TestResult } from './testResult';
 import { ResultParser } from './Parsers/ResultParser';
 import { Logger } from './logger';
+import { promisify } from 'util';
+
+const readFile = promisify(fs.readFile);
+const unlink = promisify(fs.unlink);
 
 export class TestResultsFile {
-  public parseResults(filePath: string): Promise<Array<TestResult>> {
-    return new Promise((resolve, reject) => {
-      let results: Array<TestResult>;
-      fs.readFile(filePath, (err, data) => {
-        if (!err) {
-          const xdoc = new DOMParser().parseFromString(data.toString(), 'application/xml');
-          try {
-            results = ResultParser.parseUnitTestResult(xdoc.documentElement);
+  public async parseResults(filePath: string): Promise<Array<TestResult>> {
+    let results: Array<TestResult> = [];
 
-            try {
-              fs.unlinkSync(filePath);
-            } catch {
-              Logger.LogWarning(`Unable to remove the test results file at: ${filePath}`)
-            }
+    try {
+      const data = await readFile(filePath);
+      const xdoc = new DOMParser().parseFromString(data.toString(), 'application/xml');
+      results = ResultParser.parseUnitTestResult(xdoc.documentElement);
 
-            resolve(results);
-          } catch (error) {
-            Logger.LogError('Parsing of test results failed.', error);
-            reject(error);
-          }
-        } else {
-          reject();
-        }
-      });
-    });
+      try {
+        await unlink(filePath);
+      } catch {
+        Logger.LogWarning(`Unable to remove the test results file at: ${filePath}`);
+      }
+    } catch (error) {
+      Logger.LogError('Parsing of test results failed.', error);
+    }
+
+    return results;
   }
 }
